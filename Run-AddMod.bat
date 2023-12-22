1324083cd738290e9be866537df2fabae5e18d63
Jerome226 <55724525+Jerome226@users.noreply.github.com>
2023-12-21 21:47:30 -0500

    Removed mcwifipnp.

--- Run-AddMod.bat
+++ Run-AddMod.bat
@@ -2,8 +2,17 @@
 cls
 :start
 set /p id="Mod Name: "
-java -jar Pakku-0.0.6.jar add %id%
-packwiz curseforge add %id%
+set /p co="Remove or Add [A/R] : "
+
+if /I "%co%" EQU "A" set pakku_op=add
+if /I "%co%" EQU "A" set packwiz_op=curseforge add
+if /I "%co%" EQU "A" set git_op=Added
+if /I "%co%" EQU "R" set pakku_op=rm
+if /I "%co%" EQU "R" set packwiz_op=remove
+if /I "%co%" EQU "R" set git_op=Removed
+
+java -jar Pakku-0.0.6.jar %pakku_op% %id%
+packwiz %packwiz_op% %id%
 packwiz refresh
 
 set /p ci="Commit to Git [Y/N]: "
@@ -26,7 +35,7 @@ git add ./mods/%id%.pw.toml
 git add ./index.toml
 git add ./pack.toml
 git add ./pakku-lock.json
-git commit -am "Added %id%."
+git commit -am "%git_op% %id%."
 git push
 
 java -jar Pakku-0.0.6.jar fetch

c4ab09896ee78436b8b2894d5f6dc1902f48e93b
Jerome226 <55724525+Jerome226@users.noreply.github.com>
2023-12-30 22:18:15 -0500

    Some quest work + replacing??

---
+++ kubejs/server_scripts/recipes_replace.js
@@ -0,0 +1,16 @@
+ServerEvents.recipes(event => {
+
+    function replace(i, i2) {
+        event.replaceInput({ input: i }, i, i2)
+        event.replaceOutput({ output: i }, i, i2)
+    }
+
+    replace('minecraft:wheat', 'tfc:straw')
+    replace('#forge:crops/wheat', 'tfc:straw')
+    replace('minecraft:iron_ingot', 'tfc:metal/ingot/wrought_iron')
+    replace('minecraft:gold_ingot', 'tfc:metal/ingot/gold')
+    replace('minecraft:copper_ingot', 'tfc:metal/ingot/copper')
+    replace('create:zinc_ingot', 'tfc:metal/ingot/zinc')
+    replace('create:brass_ingot', 'tfc:metal/ingot/brass')
+
+})
